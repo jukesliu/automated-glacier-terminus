54b2f4cf69c5688d25e9ea0348323c97cbdac74f
julialiu18 <julia.t.liu@gmail.com>
2020-02-27 09:49:23 -0700

    updated

--- automated_terminus_functions.py
+++ automated_terminus_functions.py
@@ -199,7 +199,7 @@ def within(value, setval, interval):
         return False
 
 
-# In[13]:
+# In[18]:
 
 
 def distance(x1, y1, x2, y2):
@@ -227,8 +227,64 @@ def midpoint(x1, y1, x2, y2):
     return midx, midy
 
 
-# In[ ]:
+# In[19]:
 
 
+def objective_func(manual_df):
+    #SPLIT INTO 3 DATAFRAMES FOR 3 FLOWLINES:
+    manual50 = manual_df[['BoxID','datetimes', 'intersect_x', 'intersect_y', 
+                                          'tpos50']].copy().reset_index(drop=True).rename(columns={"tpos50": "tpos"})
+    manual25 = manual_df[['BoxID','datetimes', 'intersect_x', 'intersect_y', 
+                                          'tpos25']].copy().reset_index(drop=True).rename(columns={"tpos25": "tpos"})
+    manual75 = manual_df[['BoxID','datetimes', 'intersect_x', 'intersect_y',
+                                          'tpos75']].copy().reset_index(drop=True).rename(columns={"tpos75": "tpos"})
+    #SIGMAS (DATA ERRORS) ALONG EACH FLOWLINE (FROM INTERANALYST DIFFERENCES)
+    sigmas = [35.02, 27.65, 30.45]; sigma_avg = np.average(sigmas);
+    
+    theta1s = []; theta2s = []
+    #FOR EACH GLACIER BOXID:
+    BoxIDs = list(set(manual_df.BoxID))
+    for BoxID in BoxIDs:
+        print("Box"+BoxID)
+        #grab automated tpos
+        auto50 = pd.read_csv(auto_path+'Tpos_Box'+BoxID+'_flowline50_filtered.csv', dtype=str,sep=',')
+        auto25 = pd.read_csv(auto_path+'Tpos_Box'+BoxID+'_flowline25_filtered.csv', dtype=str,sep=',')
+        auto75 = pd.read_csv(auto_path+'Tpos_Box'+BoxID+'_flowline75_filtered.csv', dtype=str,sep=',')
+        autodfs = [auto50, auto25, auto75]
+        #grab manual tpos that corresponds to just boxID
+        manual50_df = manual50[manual50.BoxID == BoxID].copy()
+        manual25_df = manual25[manual25.BoxID == BoxID].copy()
+        manual75_df = manual75[manual75.BoxID == BoxID].copy()
+        manualdfs = [manual50, manual25, manual75]
+        #calculate difference in terminus positions along the three flowlines
+        lists3 = []; lists3_norm = []
+        for i in range(0, len(manualdfs)):
+            man = manualdfs[i]; auto = autodfs[i]; sigma = sigmas[i]
+            compare_df = man.merge(auto, how='inner', on=['datetimes'])
+            #cast terminus positions into float values
+            compare_df = compare_df.astype({'tpos_x': 'float', 'tpos_y': 'float'})
+            #subtract the absolute value of the difference and put into df as a column named "diff"
+            compare_df['diff'] = abs(np.array(compare_df.tpos_x) - np.array(compare_df.tpos_y))  
+            compare_df['diff/sigma'] = abs(np.array(compare_df.tpos_x) - np.array(compare_df.tpos_y))/sigma
+            lists3.append(list(compare_df['diff']))  
+            lists3_norm.append(list(compare_df['diff/sigma']))
+        diff_all = lists3[0]+lists3[1]+lists3[2] #list of all the differences between manual and auto
+        normalizeddiff_all = lists3_norm[0]+lists3_norm[1]+lists3_norm[2] #list of all the normalized differences
+        N = len(diff_all) #number of total intersections
 
+        #CALCULATE THETA:
+        theta1 = (1.0/N)*np.sum(normalizeddiff_all) #sum of normalized differences along flowlines
+        theta2 = (1.0/N)*(np.sum(diff_all)/sigma_avg) #sum of differences normalized by average sigma
+        theta1s.append(theta1); theta2s.append(theta2)
+        #print("Theta values:",theta1, theta2)   
+        
+    #CALCULATE OVERALL THETA
+    theta1_all = np.average(theta1s); theta2_all = np.average(theta2s)
+    #organize data in dataframe
+    column_titles = ['Theta_avg']+BoxIDs
+    theta1_for_df = [theta1_all]+theta1s; theta2_for_df = [theta2_all]+theta2s
+    #write to csv
+    theta_df = pd.DataFrame(list(zip(column_titles, theta1_for_df, theta2_for_df)), 
+                 columns=['ID', 'theta1', 'theta2'])
+    return theta_df 
 
